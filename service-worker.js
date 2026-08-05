b6f0b0bb12fbe68526a80501ce5b394eb3d29931
provedcat <provedcat@gmail.com>
2026-08-05 15:01:15 +0900

    Unify Apps Script endpoint for feed registration

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'proved-pwa-20260805-text-request-v6';
+const CACHE_NAME = 'proved-pwa-20260805-direct-feed-registration-v7';
 const CORE_ASSETS = [
   './',
   './manifest.json',
