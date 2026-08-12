975d456c8dd76eeb1c27b1aeee2b3d31b295f7c1
provedcat <provedcat@gmail.com>
2026-08-12 14:04:41 +0900

    Remove obsolete header CSS (service-worker.js)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'proved-pwa-20260812-proved-linear-header-v4';
+const CACHE_NAME = 'proved-pwa-20260812-single-header-system-v6';
 const CORE_ASSETS = [
   './',
   './manifest.json',
